e36c366a5f5dd29525ede33bc5d731a3bf52ef7f
Tia Simmons <t.simmons06@yahoo.com>
2020-04-25 12:13:48 -0400

    started click to add project

---
+++ javascript/clickToAdd/scripts/clickToAddItems.js
@@ -0,0 +1,20 @@
+// Function's purpose - reset form fields to their natural state
+// Input - none
+// Output - none
+function clearErrors(){
+
+};
+
+// Function's purpose - clear all fields in the <form>
+// Input - none
+// Output - none
+function resetForm() {
+
+};
+
+// Function's purpose - verify the user's input is expected
+// Input - none
+// Output - none
+function validateForm() {
+
+};
